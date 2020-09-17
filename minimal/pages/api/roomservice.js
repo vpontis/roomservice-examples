308ec029134ef38909c1047005feae85ce304df6
Victor Pontis <victor@pont.is>
2020-09-17 14:19:32 -0600

    temp commit

--- minimal/pages/api/roomservice.js
+++ minimal/pages/api/roomservice.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-const API_KEY = "YOUR_API_KEY_HERE";
+const API_KEY = "U4cdUNKzQEOXQ7egQjdd3";
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
